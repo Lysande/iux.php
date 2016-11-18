3779ae600794a0590f6cf67536dc67db2350331e
Emil Johnsen <johnsen.emil@gmail.com>
2016-11-18 23:29:05 +0100

    Add folders, autoloader, peridot dependency

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * An example of a project-specific implementation.
+ *
+ * After registering this autoload function with SPL, the following line
+ * would cause the function to attempt to load the \Foo\Bar\Baz\Qux class
+ * from /path/to/project/src/Baz/Qux.php:
+ *
+ *      new \Foo\Bar\Baz\Qux;
+ *
+ * @param string $class The fully-qualified class name.
+ * @return void
+ */
+spl_autoload_register(function ($class) {
+
+    // project-specific namespace prefix
+    // $prefix = 'Foo\\Bar\\';
+    $prefix = 'iux';
+
+    // does the class use the namespace prefix?
+    $len = strlen($prefix);
+    if (strncmp($prefix, $class, $len) !== 0) {
+        // no, move to the next registered autoloader
+        return;
+    }
+
+    // get the relative class name
+
+    if ( strlen($class) > $len )
+    {
+      $relative_class = substr($class, $len);
+    }
+    else
+    {
+      $relative_class = "/" . $class;
+    }
+
+    // base directory for the namespace prefix
+    $base_dir = __DIR__ . "/lib";
+
+    // replace the namespace prefix with the base directory, replace namespace
+    // separators with directory separators in the relative class name, append
+    // with .php
+
+    $file = str_replace('\\', '/', $base_dir . $relative_class) . '.php';
+
+    // if the file exists, require it
+    if (file_exists($file)) {
+        require $file;
+    }
+});
