9f214e74f22e2cb880e811d851bcc2ed354fada1
Emil Johnsen <johnsen.emil@gmail.com>
2016-11-19 01:41:01 +0100

    Begin adding unit tests

--- lib/iux.php
+++ lib/iux.php
@@ -1,5 +1,16 @@
 <?php
 
-abstract class iux
+class iux
 {
+  protected $value;
+
+  function __construct($value)
+  {
+    $this->value = \iux\enums\Result::Ok($value);
+  }
+
+  public function get()
+  {
+    return $this->value;
+  }
 }
